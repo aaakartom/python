43a0af9e1a138eb6c1e41bc90c52df1866c41103
Artyom <artyomchvanko@gmail.com>
2022-11-12 12:19:35 -0500

    exercise14_beyond2

--- exercise14_beyond2.py
+++ exercise14_beyond2.py
@@ -2,20 +2,19 @@ database_temperatures = {'11/11': '16.5 C', '11/12': '16 C', '11/13': '7.5 C',
                          '11/14': '4 C', '11/15': '2.5 C', '11/16': '7 C', '11/17': '3.5 C'
                          }
 
+dates = list(database_temperatures.keys())
 
 user_date = input('Enter date:')
-
-
 temperature = database_temperatures.get(user_date)
+if temperature:
+    index = dates.index(user_date)
+    print(f"Current temperature: {temperature}")
 
-print(f"Current temperature: {temperature}")
-for i in range(len(list(database_temperatures))):
-    if list(database_temperatures)[i] == user_date:
-        a = i-1
-        b = i+1
-
-        if a > 0:
-            print(f'Previous temperature: {database_temperatures.get(list(database_temperatures)[a])}')
-        elif b < (len(list(database_temperatures)))-1:
-            print(f'Next temperature: {database_temperatures.get(list(database_temperatures)[b])}')
-
+    if index - 1 >= 0:
+        prev_date = dates[index - 1]
+        print(f'Previous temperature: {database_temperatures.get(prev_date)}')
+    if index + 1 < len(dates):
+        next_date = dates[index + 1]
+        print(f'Next temperature: {database_temperatures.get(next_date)}')
+else:
+    print("That date was not tracked.")
