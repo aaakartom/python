7586a176af95ebb8488344ee19a8f30e1460f39b
aaakartom <113798128+aaakartom@users.noreply.github.com>
2022-12-31 00:23:39 -0500

    Delete exercise18_beyond1.py

--- exercise18_beyond1.py
+++
@@ -1,9 +0,0 @@
-with open("exercise18_beyond1__support_file.txt", "r") as txt_file:
-    for line in txt_file:
-        file_list = line.split(' ')
-        sum_of_integers = 0
-        for word in file_list:
-            if word.isnumeric():
-                sum_of_integers += int(word)
-
-        print(sum_of_integers)
