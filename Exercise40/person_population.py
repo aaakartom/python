df9286697f10d765745ce4179a1fd1f78f4a10bd
aaakartom <artyomchvanko@gmail.com>
2023-04-08 12:01:52 -0400

    exercise 40

---
+++ Exercise40/person_population.py
@@ -0,0 +1,17 @@
+from person import Person
+from transaction import Transaction
+
+p = Person()
+
+p1 = Person()
+p2 = Person()
+p3 = Person()
+p4 = Person()
+p5 = Person()
+
+t1 = Transaction(-3000)
+t2 = Transaction(5000)
+
+print(p)
+print(p5)
+print(t1)
